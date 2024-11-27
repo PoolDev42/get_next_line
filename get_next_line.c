2144f3854a4d1325dd475d166d6556117341a634
Luis Calero <luis.calero1777@gmail.com>
2024-11-27 22:53:29 +0100

    setup get next line

---
+++ get_next_line.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcalero <lcalero@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 20:20:00 by lcalero           #+#    #+#             */
+/*   Updated: 2024/11/27 22:43:43 by lcalero          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char		*buffer;
+	static char	*line;
+	size_t		i;
+
+	i = 0;
+	read(fd, buffer, BUFFER_SIZE);
+}
